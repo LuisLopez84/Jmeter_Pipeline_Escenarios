d9b2096fd56dc9e422067a560113f99e728e41db
Luis Lopez <ingluislopezh@outlook.com>
2025-06-19 09:07:48 -0500

    primer commit

---
+++ BAT_PRUEBA.bat
@@ -0,0 +1,6 @@
+﻿REM Inicia
+for /L %%a IN (%2, 1, %3) DO ( 
+mkdir %1\Escenario%%a
+mkdir %1\Escenario%%a\Dashboard
+jmeter -n -t %1\Escenario%%a.jmx -l %1\Escenario%%a\Escenario%%a.jtl -e -o %1\Escenario%%a\DashBoard
+)
